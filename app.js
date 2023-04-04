6fa28b890b71372cd288e87bb54dc2e9a13a68d8
kokoroTyan <102168174+kokoroTyan@users.noreply.github.com>
2023-04-04 18:15:22 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ AOS.init();
 function load() {
     let preloader = document.getElementById('preloader');
     setTimeout(() => {preloader.classList.add("loaded")}, 1400);
+    // preloader.style.position = "static";
 }
 
 document.addEventListener("DOMContentLoaded", load);
