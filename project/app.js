c2702018f308f14e0297d5a409294fcb8dc8dc4a
kokoroTyan <102168174+kokoroTyan@users.noreply.github.com>
2023-04-04 18:19:10 +0300

    Delete project directory

--- project/app.js
+++
@@ -1,8 +0,0 @@
-AOS.init();
-
-function load() {
-    let preloader = document.getElementById('preloader');
-    setTimeout(() => {preloader.classList.add("loaded")}, 1400);
-}
-
-document.addEventListener("DOMContentLoaded", load);
